cebe9f0021d8dda429a02c180ee81ad621385c1c
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-11-06 01:55:36 +0530

    fixed blank results

--- src/components/ResultBox.jsx
+++ src/components/ResultBox.jsx
@@ -5,18 +5,26 @@ import { Typography } from "@mui/material";
 const ResultBox = ({ value: { bmi, status }, color }) => {
   return (
     <>
-      <Typography variant="subtitle1" sx={{ mt: 3 }}>
-        Your BMI is{" "}
-        <Typography sx={{ display: "inline-block", fontWeight: "bold", color }}>
-          {bmi}
-        </Typography>
-      </Typography>
-      <Typography variant="subtitle1">
-        You are currently{" "}
-        <Typography sx={{ display: "inline-block", fontWeight: "bold", color }}>
-          {status}
-        </Typography>
-      </Typography>
+      {bmi && status ? (
+        <>
+          <Typography variant="subtitle1" sx={{ mt: 3 }}>
+            Your BMI is{" "}
+            <Typography
+              sx={{ display: "inline-block", fontWeight: "bold", color }}
+            >
+              {bmi}
+            </Typography>
+          </Typography>
+          <Typography variant="subtitle1">
+            You are currently{" "}
+            <Typography
+              sx={{ display: "inline-block", fontWeight: "bold", color }}
+            >
+              {status}
+            </Typography>
+          </Typography>
+        </>
+      ) : null}
     </>
   );
 };
